3f370a33d370f89a36faedc57e3f9bab749d24d4
CORP\pinhoa <andre.pinho@webdisplay.pt>
2020-01-14 20:39:13 +0000

    Put a pin in his location

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,6 +30,10 @@ const IndexPage = () => {
       console.log(`Failed to find Santa!: ${e}`);
     }
     console.log("routeJson", routeJson);
+    const { destinations = [] } = routeJson || {};
+    const destinationsVisited = destinations.filter(({arrival}) => arrival < Date.now());
+    const destinationsWithPresents = destinationsVisited.filter(({presentsDelivered}) => presentsDelivered > 0);
+    const lastKnownDestination = destinationsWithPresents[destinationsWithPresents.length - 1];
   }
 
   const mapSettings = {
